73dd14c97c70a57b6660bb3f1251eb5474eda21d
mdesignbymaxi <mdesignbymaxi@gmail.com>
2025-03-06 17:20:02 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Mobile Menu Toggle
-const navToggle = document.querySelector('.nav-toggle');
-const navMenu = document.querySelector('.nav-menu');
-
-navToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-});
